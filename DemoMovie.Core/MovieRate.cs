b10e8d367cfca5fbd07f85c69cc5e7c35d5b459f
resul.turfent <resul.turfent@BDH-5CG0376FTK>
2022-12-04 22:24:34 +0300

    MovieRate tablosu eklendi

---
+++ DemoMovie.Core/MovieRate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMovie.Core
+{
+    public class MovieRate
+    {
+        public string Comment { get; set; }
+        public byte Score { get; set; }
+        public int UserId { get; set; }
+        public int MoviesId { get; set; }
+        public Movies Movies { get; set; }
+        public User User { get; set; }
+    }
+}
