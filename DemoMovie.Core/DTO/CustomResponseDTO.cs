5b6cda4715f82a0c8780f5e73c9c80c9daf990be
resul.turfent <resul.turfent@BDH-5CG0376FTK>
2022-12-04 18:45:29 +0300

    Response yapıları eklendi

---
+++ DemoMovie.Core/DTO/CustomResponseDTO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DemoMovie.Core.DTO
+{
+    public class CustomResponseDTO<T>
+    {
+
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<string> Errors { get; set; }
+
+
+        public static CustomResponseDTO<T> Success(int statusCode, T data)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode, Data = data };
+        }
+
+        public static CustomResponseDTO<T> Success(int statusCode)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode };
+        }
+
+        public static CustomResponseDTO<T> Fail(int statusCode, string errors)
+        {
+            return new CustomResponseDTO<T> { StatusCode = statusCode, Errors = new List<string> { errors } };
+        }
+
+
+    }
+}
